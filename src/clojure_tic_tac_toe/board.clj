14dbc15d00f5bfa703239694e186f63ac58beeb0
Jon Chin <jchin@8thlight.com>
2017-12-04 08:57:02 -0800

    Test board size of 4x4

--- src/clojure_tic_tac_toe/board.clj
+++ src/clojure_tic_tac_toe/board.clj
@@ -2,6 +2,11 @@
   (:require [clojure.set :as set]
             [clojure-tic-tac-toe.utilities :as utilities]))
 
+;(def valid-moves #{:1 :2 :3 :4
+;                   :5 :6 :7 :8
+;                   :9 :10 :11 :12
+;                   :13 :14 :15 :16})
+
 (def valid-moves #{:1 :2 :3
                    :4 :5 :6
                    :7 :8 :9})
--- src/clojure_tic_tac_toe/tic_tac_toe_rules.clj
+++ src/clojure_tic_tac_toe/tic_tac_toe_rules.clj
@@ -1,5 +1,21 @@
 (ns clojure-tic-tac-toe.tic_tac_toe_rules)
 
+;(def winning-rows
+;  (list #{:1 :2 :3 :4}
+;        #{:5 :6 :7 :8}
+;        #{:9 :10 :11 :12}
+;        #{:13 :14 :15 :16}))
+
+;(def winning-columns
+;  (list #{:1 :5 :9 :13}
+;        #{:2 :6 :10 :14}
+;        #{:3 :7 :11 :15}
+;        #{:4 :8 :12 :16}))
+
+;(def winning-diagonals
+;  (list #{:1 :6 :11 :16}
+;        #{:4 :7 :10 :13}))
+
 (def winning-rows
   (list #{:1 :2 :3}
         #{:4 :5 :6}
